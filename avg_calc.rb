f1c576f325495eff13e9c6da16b87462bd45afc1
Brian Bensky <brianbensky@gmail.com>
2017-07-14 16:44:53 -0600

    refactored number display to method

--- avg_calc.rb
+++ avg_calc.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-helpers do
-  def integer?(num)
-    num.to_i == num
-  end
+def integer?(num)
+  num.to_i == num
+end
+
+def format_number(num)
+  rounded_num = num.round(2)
+  integer?(rounded_num) ? rounded_num.to_i : rounded_num
 end
 
 get '/' do
   numbers = params[:query].split(/\W+/).map(&:to_i) unless !params[:query]
 
   unless numbers.nil? || numbers.empty?
-    @average = (numbers.reduce(:+) / numbers.size.to_f).round(2)
+    average = numbers.reduce(:+) / numbers.size.to_f
+    @formatted_average = format_number(average)
   end
   
   erb :index
